20ca0429d3fbc1e052a8beaf58b6079f22c53079
neotao <neotao@126.com>
2020-05-07 14:49:36 +0800

    crazyit java源码

---
+++ crazy-java-3rd/codes/src/main/java/07/7.5/StringReg.java
@@ -0,0 +1,29 @@
+
+import java.util.*;
+/**
+ * Description:
+ * <br/>网站: <a href="http://www.crazyit.org">疯狂Java联盟</a>
+ * <br/>Copyright (C), 2001-2016, Yeeku.H.Lee
+ * <br/>This program is protected by copyright laws.
+ * <br/>Program Name:
+ * <br/>Date:
+ * @author Yeeku.H.Lee kongyeeku@163.com
+ * @version 1.0
+ */
+public class StringReg
+{
+	public static void main(String[] args)
+	{
+		String[] msgs =
+		{
+			"Java has regular expressions in 1.4",
+			"regular expressions now expressing in Java",
+			"Java represses oracular expressions"
+		};
+		for (String msg : msgs)
+		{
+			System.out.println(msg.replaceFirst("re\\w*" , "哈哈:)"));
+			System.out.println(Arrays.toString(msg.split(" ")));
+		}
+	}
+}
